e1bf925e80a44f7e1b33d6c48679f0a2d9facca9
karthik9392 <119335980+karthik9392@users.noreply.github.com>
2024-04-24 12:19:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+function Validate() {
+  var password = document.getElementById("signup_password").value;
+  var confirmPassword = document.getElementById("confirm_password").value;
+
+  if (password != confirmPassword) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  }
+   else if (confirmPassword == "" || password == "") {
+
+  }
+  else{
+    confirm_password.setCustomValidity("");
+  }
+}  
+
+
+function authenticate() {
+  const username = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  // Replace with your actual authentication logic
+  if (username === "admin" && password === "secret") {
+      window.location.href = "index.html"; // Redirect to index page
+  } else {
+      alert("Invalid credentials. Please try again.");
+  }
+}
+  
+  
+  let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+  document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+  document.querySelector('#search-form').classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop:true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop:true,
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
+
+
